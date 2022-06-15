d981227b9d1c7a7479ec7c08f302d89fd45c972a
Youssef T <sheese.711@gmail.com>
2022-06-15 14:31:59 +0200

    project setup and the header buttons

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function TopButtons() {
+    const cities = [
+        {
+            id: 1,
+            title: "London",
+        },
+        {
+            id: 2,
+            title: "Paris",
+        },
+        {
+            id: 3,
+            title: "Berlin",
+        },
+        {
+            id: 4,
+            title: "Madrid",
+        },
+        {
+            id: 5,
+            title: "Rome",
+        },
+    ]
+  return (
+    
+    
+
+    <div className="flex items-center justify-around my-6">
+        {cities.map(city => ( 
+            <button className="text-white text-lg font-medium" key={city.id}>{city.title}</button> 
+        ))}
+    </div>
+  )
+}
+
+export default TopButtons
