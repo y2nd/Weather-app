c955266465041a48ca2df5f0799b5b33e98248d6
Youssef T <sheese.711@gmail.com>
2022-06-16 16:47:14 +0200

    fetching data for temperature and details is done

--- src/App.js
+++ src/App.js
@@ -6,25 +6,41 @@ import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forcast from './components/Forcast';
 import getFormattedWeatherData from './services/WeatherService';
 
+import {useEffect, useState} from 'react';
+
 function App() {
 
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({q: "london"});
-    console.log(data);
-  }
+  const [query, setQuery] = useState({q: "rabat"});
 
-  fetchWeather();
+  const [units, setUnits] = useState("metric");
+
+  const [weather, setWeather] = useState(null);
+
+  useEffect( () => {
+    const fetchWeather = async () => {
+      const data = await getFormattedWeatherData({...query, units}).then((data) => {setWeather(data)});
+
+      console.log(weather);
+    }
+  
+    fetchWeather();
+  }, [query, units]);
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons></TopButtons>
       <Inputs></Inputs>
 
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather}/>
+          <TemperatureAndDetails weather={weather}/>
+          <Forcast title="hourly forcast"/>
+          <Forcast title="daily forcast"/>
+        </>
+      )}
+
       
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-      <Forcast title="hourly forcast"/>
-      <Forcast title="daily forcast"/>
     </div>
   );
 }
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { formatToLocalTime } from '../services/WeatherService'
 
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return (
     <div>
         <div className="flex items-center justify-center my-6">
 
             <p className="text-white text-xl font-extralight">
-                Tuesday, 31 MAY 2022 | Local time: 12:00 PM
+              {formatToLocalTime(dt, timezone)}
             </p>
 
         </div>
 
         <div className="flex items-center justify-center my-6">
-            <p className="text-white text-3xl font-medium">Berlin, DE</p>
+            <p className="text-white text-3xl font-medium">{`${name}, ${country}`}</p>
         </div>
     </div>
   )
