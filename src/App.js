2db17a6672947a7db1f0fe055e9996fd002669a2
Youssef T <sheese.711@gmail.com>
2022-06-17 10:20:36 +0200

    fetching data complete

--- src/App.js
+++ src/App.js
@@ -26,8 +26,18 @@ function App() {
     fetchWeather();
   }, [query, units]);
 
+  
+  const formatBackground = () => {
+    if(!weather) return "from-cyan-700 to-blue-700";
+    const treshhold = units === "metric" ? 25 : 77;
+    if(weather.temp <= treshhold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
+
+
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
       <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -9,6 +9,17 @@ function Inputs( {setQuery, units, setUnits} ) {
     if( city !== "" ) setQuery ({q: city})
   };
 
+  const handleKeyPress = (e) => {
+    if( e.key === "Enter" ) {
+      if( city !== "" ) setQuery ({q: city})
+    }
+  }
+
+  const handleUnitsChange = (e) => {
+    const selectedUnits = e.currentTarget.name;
+    if(units != selectedUnits) setUnits(selectedUnits);
+  }
+
   const handleLocationClick = () => {
     if( navigator.geolocation ) {
       navigator.geolocation.getCurrentPosition( position => {
@@ -23,19 +34,20 @@ function Inputs( {setQuery, units, setUnits} ) {
     }
   }
 
+
   return (
     <div className="flex flex-row justify-center my-6">
         <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-            <input value={city} onChange={(e) => setCity(e.currentTarget.value)}  type="text" className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" placeholder="Search for city..."/>
+            <input onKeyPress={handleKeyPress} value={city} onChange={(e) => setCity(e.currentTarget.value)}  type="text" className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" placeholder="Search for city..."/>
 
             <UilSearch onClick={handleSearchClick} className="text-white cursor-pointer transition ease-out hover:scale-125" size={25} />
             <UilLocationPoint onClick={handleLocationClick} className="text-white cursor-pointer transition ease-out hover:scale-125" size={25}/>
         </div>
 
         <div className="flex flex-row w-1/4 items-center justify-center ">
-            <button onClick={() => setUnits("metric")} name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125">°C</button>
+            <button onClick={handleUnitsChange} name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125">°C</button>
             <span className="text-xl text-white mx-1" >|</span>
-            <button onClick={() => setUnits("imperial")} name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125">°F</button>
+            <button onClick={handleUnitsChange} name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125">°F</button>
         </div>
 
     </div>
